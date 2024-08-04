b1b54b26629f8f261d99ed402a760eac2fcfd5cf
icebird <22712286+kevinchien920819@users.noreply.github.com>
2024-08-04 23:45:40 +0800

    getinstruction feature developing

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,4 +5,5 @@ static final globalNavigatorKey = GlobalKey<NavigatorState>();
 static const googleApiKey = "AIzaSyCaYbgYRGrbzXI-dajsD9FMwesDnkxcLIk";
 static const darkMapStyleJson = 'assets/json/dark_mode_style.json';
 static const androidLocationIntentAddress = 'android.settings.LOCATION_SOURCE_SETTINGS';
+static const mapboxApiKey = "pk.eyJ1IjoiaWNlYmlyZCIsImEiOiJjbHlkczRndGIwOTJsMmpwdjhobnkwcDl4In0.6ROt8LHsbEPwal19GSCmKw";
 }
